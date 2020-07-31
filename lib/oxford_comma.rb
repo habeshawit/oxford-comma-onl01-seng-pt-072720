3e3ebbe6cab24ec27de834480bed7f0a1c7361dc
Shalom Kebede <shalomseleshi@gmail.com>
2020-07-31 22:06:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    return array
+    array.to_s
   elsif array.length ==2
-    return array.join(' and ')
+    array.join(' and ')
   else
     array[-1] = "and #{array[-1]}"
     array.join(', ')
 end
+
+return array.to_s if array.nil? or array.length <= 1
